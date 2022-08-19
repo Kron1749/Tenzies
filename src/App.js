1c7c74950ca7bbcf4d5a209934f4ccebed7f63ae
Kron1749 <drakgr98@gmail.com>
2022-08-19 10:16:54 +0300

    Created Die Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from "react";
+import Die from "./Components/Die";
 import "./style.css";
 
 export default function App() {
-  return <main></main>;
+  return (
+    <main>
+      <div className="dice-container">
+        <Die value="1" />
+        <Die value="2" />
+        <Die value="3" />
+        <Die value="4" />
+        <Die value="5" />
+        <Die value="6" />
+        <Die value="7" />
+        <Die value="8" />
+        <Die value="9" />
+        <Die value="10" />
+      </div>
+    </main>
+  );
 }
