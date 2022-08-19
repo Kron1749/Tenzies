cf6badace1bdd6d2ac457a41b4c3cf3521f92051
Kron1749 <drakgr98@gmail.com>
2022-08-19 10:59:29 +0300

    Styling dice

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React from "react";
 import Die from "./Components/Die";
+import { nanoid } from "nanoid";
 import "./style.css";
 
 export default function App() {
   const [dice, setDice] = React.useState(allNewDice());
+
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.random() * 6));
+      newDice.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid(),
+      });
     }
+
     return newDice;
   }
 
-  const diceElements = dice.map((die) => <Die value={die} />);
+  function generate() {
+    setDice(allNewDice());
+  }
+  const diceElements = dice.map((die) => (
+    <Die key={die.id} value={die.value} isHeld={die.isHeld} />
+  ));
 
   return (
     <main>
       <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={generate}>
+        Roll
+      </button>
     </main>
   );
 }
